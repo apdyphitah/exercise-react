06024cc4e36ca5e548f5851ee275a86a869a8894
dige <maqdashaawi@gmail.com>
2026-06-06 13:24:57 +0300

    ex1

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserCard from './assets/ex1/UserCard'
+function App() {
+
+  return (
+    <>
+      <UserCard />
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/assets/ex1/UserCard.jsx
@@ -0,0 +1,14 @@
+
+
+function UserCard() {
+  return (
+    <>
+        <h2>abdifatah dige</h2>
+        <p>apdyphitah1@gmail.com</p>
+    
+    </>
+    
+  )
+}
+
+export default UserCard
