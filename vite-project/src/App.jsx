f25776640ba7f30c226495317e2cf8bb155ebef0
dige <maqdashaawi@gmail.com>
2026-06-06 15:23:39 +0300

    ex3

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Blog from './assets/ex2/Blog'
+import UserCard from './assets/ex3/UserCard'
 function App() {
 
   return (
+   <>   
+    <UserCard name="abdifatah dige" email="abdifatah@example.com" />
+    <UserCard name= "ahmed mohamed" email="ahmed@example.com" />
    
-      <Blog />
+   </>
+     
    
   )
 }
---
+++ vite-project/src/assets/ex3/UserCard.jsx
@@ -0,0 +1,10 @@
+const UserCard = ({name, email}) => {
+  return (
+    <>
+        <h2>name: {name}</h2>
+        <p>email: {email}</p>
+    </>
+  )
+}
+
+export default UserCard
